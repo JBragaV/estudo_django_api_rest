128cdb3e57dd6214ea123893ae1e0e35d34e34e9
JBragaV <44869203+JBragaV@users.noreply.github.com>
2021-04-12 22:00:40 -0300

    Atualização das rotas

--- dispensa/urls.py
+++ dispensa/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
 from django.urls import path
 from .views import *
 
-urlpatterns = [
-    path('dispensa/', DispensaList.as_view()),
-    path('dispensa/<int:id>', DispensaDetalhes.as_view())
-]
+
+
+"""
+Jeito diferente de criar rota
+path('api/', include('dispensa.urls')) setup.urls em urlpatterns
+"""
--- setup/urls.py
+++ setup/urls.py
@@ -1,22 +1,16 @@
-"""setup URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from dispensa.views import DispensaViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('dispensa', DispensaViewSet, basename='dispensa')
+router.register('lista_compras', DispensaViewSet, basename='lista')
+router.register('jogos', DispensaViewSet, basename='jogos')
+router.register('livros', DispensaViewSet, basename='livros')
+router.register('pessoas', DispensaViewSet, basename='pessoas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('dispensa.urls'))
+    path('', include(router.urls)),
 ]
